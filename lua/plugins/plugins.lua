012f75310f3793ebf8cf840f9fc5c17de006941d
jsonMartin <4033621+jsonMartin@users.noreply.github.com>
2024-01-20 13:23:33 -0800

    Add Tmux navigator integration

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -32,4 +32,23 @@ return {
       },
     },
   },
+
+  -- TMUX & VIM integration
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
 }

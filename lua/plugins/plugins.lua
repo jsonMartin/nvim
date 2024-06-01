2a61e90b12219d50357e032ce3c100839a59704d
jsonMartin <4033621+jsonMartin@users.noreply.github.com>
2024-05-31 20:38:13 -0700

    latest update

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,19 +1,40 @@
+local function biome_lsp_or_prettier(bufnr)
+  local has_biome_lsp = vim.lsp.get_active_clients({
+    bufnr = bufnr,
+    name = "biome",
+  })[1]
+  if has_biome_lsp then
+    return {}
+  end
+  local has_prettier = vim.fs.find({
+    -- https://prettier.io/docs/en/configuration.html
+    ".prettierrc",
+    ".prettierrc.json",
+    ".prettierrc.yml",
+    ".prettierrc.yaml",
+    ".prettierrc.json5",
+    ".prettierrc.js",
+    ".prettierrc.cjs",
+    ".prettierrc.toml",
+    "prettier.config.js",
+    "prettier.config.cjs",
+  }, { upward = true })[1]
+  if has_prettier then
+    return { "prettier" }
+  end
+  return { "biome" }
+end
+
 return {
   -- Use Biome instead of prettier / eslint
-  -- {
-  --   "stevearc/conform.nvim",
-  --   enable = false,
-  -- },
-
   {
     "stevearc/conform.nvim",
-    optional = true,
     opts = {
       formatters_by_ft = {
-        ["javascript"] = { "biome" },
-        ["javascriptreact"] = { "biome" },
-        ["typescript"] = { "biome" },
-        ["typescriptreact"] = { "biome" },
+        ["javascript"] = biome_lsp_or_prettier,
+        ["javascriptreact"] = biome_lsp_or_prettier,
+        ["typescript"] = biome_lsp_or_prettier,
+        ["typescriptreact"] = biome_lsp_or_prettier,
         ["vue"] = { "biome" },
         ["css"] = { "biome" },
         ["scss"] = { "biome" },
